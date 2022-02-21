8bdd1ed729da48c0d418b25a6d247528f84ec5b2
angularsantanderii <angularsantanderii@gmail.com>
2022-02-21 11:32:04 +0000

    components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'p04estructura';
+  
+  mensaje: string = '¡Hola Mundo!';
+
 }
